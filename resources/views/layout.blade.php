d0004b33e64a395e8e2d3b1c397e1c1cf88ca63b
unknown <zulatto.yve@gmail.com>
2022-03-20 17:27:06 -0300

    create nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
-    <!-- <link rel="stylesheet" href="css/style.css"> -->
-    <title>teste</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>LearnBetter</title>
 </head>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,49 @@
 @extends('layout')
 
     <body>
+        <header class="header" id="header">
+            <nav class="nav container">
+                <a href="#" class="nav_logo">BetterMentory</a>
 
+                <div class="nav_menu" id="nav-menu">
+                    <ul class="nav_list grid">
+                        <!-- list itens menu -->
+                        <li class="nav_item">
+                            <a href="#home" class="nav_link" id="active-link">
+                                Home
+                            </a>
+                        </li>
+                         
+                        <li class="nav_item">
+                            <a href="#platform" class="nav_link">
+                                Plataforma
+                            </a>
+                        </li>
+
+                        <li class="nav_item">
+                            <a href="#mentors" class="nav_link">
+                                Mentores
+                            </a>
+                        </li>
+
+                        <li class="nav_item">
+                            <a href="#contact" class="nav_link btn_menu">
+                                Contate-nos
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="nav_btns">
+                    <!-- theme change btn -->
+                    <i class="uil uil-moon change-theme" id="theme-button"></i>
+
+                    <div class="nav_toggle" id="nav-toggle">
+                        <i class="uil uil-apps"></i>
+                    </div>
+                </div>
+            </nav>
+        </header>
     </body>
 </html>
 
